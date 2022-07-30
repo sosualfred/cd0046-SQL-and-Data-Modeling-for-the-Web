7839acbda811e74ec3781b8bd18a23bd02800093
sosualfred <sosualfred@gmail.com>
2022-07-30 16:33:56 +0000

    move code to models.py

---
+++ models.py
@@ -0,0 +1,59 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# ----------------------------------------------------------------------------#
+# Models.
+# ----------------------------------------------------------------------------#
+
+
+class Venue(db.Model):
+    __tablename__ = 'venues'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.String(120), nullable=False)
+    facebook_link = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, default=False)
+    description = db.Column(db.String(1500))
+    shows = db.relationship('Show', backref='venue', lazy=True)
+
+
+artist_show = db.Table('artist_show',
+                       db.Column('artist_id', db.Integer,
+                                 db.ForeignKey('artists.id')),
+                       db.Column('show_id', db.Integer,
+                                 db.ForeignKey('shows.id'))
+                       )
+
+
+class Artist(db.Model):
+    __tablename__ = 'artists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.String(120), nullable=False)
+    facebook_link = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    description = db.Column(db.String(1500))
+    shows = db.relationship('Show', backref='artist', lazy=True)
+
+
+class Show(db.Model):
+    __tablename__ = 'shows'
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey(
+        'artists.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey(
+        'venues.id'), nullable=False)
+    start_time = db.Column(db.DateTime, nullable=False)
+    artists = db.relationship('Artist', secondary=artist_show, lazy=True)
